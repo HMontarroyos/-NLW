b352fb3979fa0d1622d37cc5bf2dc54ce64d868f
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2020-06-03 08:58:43 -0300

    Olhando as Oportunidades - Aula 2

---
+++ scripts/create-point.js
@@ -0,0 +1,107 @@
+function populateUFs(){
+
+    const ufSelect = document.querySelector ( "select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then(res => res.json() )
+    .then( states =>   {
+
+             for(const state of states ){
+                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+             }
+             
+
+        } )
+}
+
+populateUFs()
+
+function getCities(event){
+
+    const citySelect = document.querySelector ( "[name=city]")
+    const stateInput = document.querySelector ( "[name=state]")
+
+const ufValue = event.target.value
+
+const indexOfSelectedState = event.target.selectedIndex
+stateInput.value = event.target.options[indexOfSelectedState].text
+
+const url= `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+fetch(url)
+    .then(res => res.json() )
+    .then( cities =>   {
+
+             for(const city of cities ){
+                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+             }
+
+
+             citySelect.disabled = false
+             
+
+        } )
+
+}
+
+
+
+document.querySelector("select[name =uf]")
+.addEventListener("change" , getCities)
+
+
+
+/*
+
+faltou o function case sensitive 
+faltou o igual no value ao passar o dollar 
+
+function populateufs(){
+
+    const ufSelect = document.querySelector ( "select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then(res => res.json() )
+    .then( states =>   {
+
+             for(const state of states ){
+                ufSelect.innerHTML += `<option value"${state.id}">${state.nome}</option>`
+             }
+             
+
+        } )
+}
+
+populateufs()
+
+
+
+
+function getCities(event){
+
+    const citySelect = document.querySelector ( "select[name=city]")
+
+const ufValue = event.target.value
+
+const url= `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+fetch(url)
+    .then(res => res.json() )
+    .then( cities =>   {
+
+             for(const city of cities ){
+                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+             }
+
+
+             citySelect.disabled = false
+             
+
+        } )
+
+}
+
+
+
+document.querySelector("select[name =uf]")
+.addEventListener("change" , getCities)*/
