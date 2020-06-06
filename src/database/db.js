918a01b115bd11ba5d88c93cc949c1b49d970925
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2020-06-06 13:53:36 -0300

    Banco de Dados Teste

--- src/database/db.js
+++ src/database/db.js
@@ -1,95 +1,185 @@
-// importar a dependencia do sqlite3 
-
-const sqlite3 = require("sqlite3").verbose()
-
-// criar o objeto que irá fazer operações no banco de dados 
-
+// imporotar dependencia do sqlite
+const sqlite3 = require ("sqlite3").verbose()
+//criar e iniciar o objeto de banco de dados para realizar as operações
 const db = new sqlite3.Database("./src/database/database.db")
-
-// Utilizar o objeto de banco de dados, para nossas operações 
-
-module.exports = db 
-
-//PARA RODAR FAVOR UTILIZAR O SEGUINTE COMANDO : 
-// node src/database/db.js
-
-// E DESCOMENTAR QUAL AÇÃO IRA UTILIZAR EX. DELETE OU PUT ETC ...
-
-
-/*
-db.serialize(() => {
-
-    // com comandos SQL eu vou :
-
-    // 1 Criar uma tabela 
-
+//vamos utilizar o objeto de banco de dados para realizar as operações
+/*db.serialize(() => {
+    //criar a  tabela via SQL
     db.run(`
-        CREATE TABLE IF NOT EXISTS places (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            image TEXT,
+        CREATE TABLE IF NOT EXISTS places(
+            id  INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
+            image TEXT,
             address TEXT,
             address2 TEXT,
             state TEXT,
             city TEXT,
             items TEXT
-
         );
     `)
-    // 2 Inserir dados na tabela 
-const query = `
-    INSERT INTO places (
-        image,
-        name,
-        address,
-        address2,
-        state,
-        city,
-        items 
-    ) VALUES (?,?,?,?,?,?,?);
-`
-const values = [
-    "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-    "Paperside",
-    "Guilherme Gemballa, Jardim América",
-    "Número 260",
-    "Santa Catarina",
-    "Rio Sul",
-    "Papeis e Papelão"
-]
-
-function afterInsertData(err){
-    if(err) {
-        return console.log(err)
+    //inserir dados
+    const query = `INSERT INTO places (name,image,address,address2,state,city,items) VALUES (?,?,?,?,?,?,?);`
+    const values = [
+        "Paper Sider",
+        "https://images.unsplash.com/photo-1503596476-1c12a8ba09a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        "Guilherme Gemballa",
+        "Jardim América, N° 260",
+        "Santa Catarina",
+        "Rio do Sul",
+        "Papéis, Papelão"
+    ]
+    function affterInsertData(err) {
+        if (err){
+            return console.log(err)
+        }
+        console.log ("Cadastrado com Sucesso")
+        console.log(this)
     }
-    console.log("Cadastrado com sucesso ")
-    console.log(this)
-}
-
-    db.run(query,values,afterInsertData)
-
-    // 3 Consultar os dados da tabela 
-
-    db.all(`SELECT * FROM places`,function (err, rows) {
-                     if (err) {
-                        return console.log(err)
-                    }
-        
-                    console.log("Aqui estão seus registros: ")
-                     console.log(rows)
-                })
-
-        // 4 Deletar um dado da tabela 
-
-            db.run(`DELETE FROM places WHERE id = ?`, [1], function (err) {
-                 if(err) {
-                     return console.log(err)
-                 }
     
-                 console.log("Registro foi apagado com sucesso")
-             })
+    
+    db.run(query, values, affterInsertData)
+    //consultar dados
+    db.all(`SELECT * FROM places`, function(err,rows){
+        if (err){
+            return console.log(err)
+        }
+        console.log("Registros encontrados")
+        console.log(rows)
+    })
+    //excluir dados
+    db.run(`DELETE FROM places WHERE id=?`,[1], function(err){
+        if (err){
+            return console.log(err)
+        }
+        console.log("Registro deletado com sucesso")
+    })
+    db.all(`SELECT * FROM places`, function(err,rows){
+        if (err){
+            return console.log(err)
+        }
+        console.log("Registros encontrados")
+        console.log(rows)
+    })
+})*/
+
+
+//exportar obj para ser usado em outro local
+module.exports = db
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // importar a dependencia do sqlite3 
+
+// const sqlite3 = require("sqlite3").verbose()
+
+// // criar o objeto que irá fazer operações no banco de dados 
+
+// const db = new sqlite3.Database("./src/database/database.db")
+
+// // Utilizar o objeto de banco de dados, para nossas operações 
+
+// module.exports = db 
+
+// //PARA RODAR FAVOR UTILIZAR O SEGUINTE COMANDO : 
+// // node src/database/db.js
+
+// // E DESCOMENTAR QUAL AÇÃO IRA UTILIZAR EX. DELETE OU PUT ETC ...
+
+
+
+// // db.serialize(() => {
+
+// //     // com comandos SQL eu vou :
+
+// //     // 1 Criar uma tabela 
+
+// //     db.run(`
+// //         CREATE TABLE IF NOT EXISTS places (
+// //             id INTEGER PRIMARY KEY AUTOINCREMENT,
+// //             image TEXT,
+// //             name TEXT,
+// //             address TEXT,
+// //             address2 TEXT,
+// //             state TEXT,
+// //             city TEXT,
+// //             items TEXT
+
+// //         );
+// //     `)
+// //     // 2 Inserir dados na tabela 
+// // const query = `
+// //     INSERT INTO places (
+// //         image,
+// //         name,
+// //         address,
+// //         address2,
+// //         state,
+// //         city,
+// //         items 
+// //     ) VALUES (?,?,?,?,?,?,?);
+// // `
+// // const values = [
+// //     "https://images.unsplash.com/photo-1567393528677-d6adae7d4a0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+// //     "Paperside",
+// //     "Guilherme Gemballa, Jardim América",
+// //     "Número 260",
+// //     "Santa Catarina",
+// //     "Rio Sul",
+// //     "Papeis e Papelão"
+// // ]
+
+// // function afterInsertData(err){
+// //     if(err) {
+// //         return console.log(err)
+// //     }
+// //     console.log("Cadastrado com sucesso ")
+// //     console.log(this)
+// // }
+// //     //Adiciona dados na tabela 
+
+// //     db.run(query,values,afterInsertData)
+
+// //     // 3 Consultar os dados da tabela 
+
+// //     db.all(`SELECT * FROM places`,function (err, rows) {
+// //                      if (err) {
+// //                         return console.log(err)
+// //                     }
+        
+// //                     console.log("Aqui estão seus registros: ")
+// //                      console.log(rows)
+// //                 })
+
+// //         // 4 Deletar um dado da tabela 
+
+// //             db.run(`DELETE FROM places WHERE id = ?`, [1], function (err) {
+// //                  if(err) {
+// //                      return console.log(err)
+// //                  }
+    
+// //                  console.log("Registro foi apagado com sucesso")
+// //              })
      
 
-})
+// // })
 
-*/
+
--- src/server.js
+++ src/server.js
@@ -6,8 +6,6 @@
 
 // const db = require("./database/db.js")
 
-
-
 // server.use(express.static("public"))
 
 // // habilitar o uso do req.body na aplicação 
@@ -106,95 +104,88 @@
 
 
 // // ligar o servidor 
-// server.listen(3000)
+// server.listen(2000)
+
 
 const express = require("express")
 const server = express()
 
-const db = require("./database/db.js")
+//pegar banco de dados
+const db = require("./database/db")
 
+//Configurar Pasta Public
 server.use(express.static("public"))
 
-server.use(express.urlencoded({ extended: true }))
+//habilitar o uso de rq.body
+server.use(express.urlencoded ( { extended:true } ) )
+//usando template engine nunjucks
+    const nunjucks= require("nunjucks")
 
-const nunjucks = require("nunjucks")
-nunjucks.configure("src/views", {
-    express: server,
-    noCache: true
-})
+    nunjucks.configure("src/views", {
+        express: server,
+        noCache: true
+    })
 
-server.get("//", (req, res) => {
-    return res.render("index.html")
-})
+//configurar rotas
+// >>>> HOME
 
-server.get("/create-point", (req, res) => {
-    return res.render("create-point.html")
-})
+    server.get("/", (req, res) => {
+       return  res.render("index.html", {title: "Seu Market Place de Coleta de resíduos"})    
+    })
+    
+    server.get("/create-point", (req, res) => {
+        //req.query: Query strings
+        console.log(req.query)
+        return res.render("create-point.html")    
 
-server.post("/savepoint", (req, res) => {
-         const query=` 
-            INSERT INTO places (
-          
-                image,
-                name,
-                address,
-                address2,
-                state,
-                city,
-                items
-            ) VALUES (?,?,?,?,?,?,?);
-        `
+    })
+    server.post("/savepoint", (req,res) => {
+        //req.body: o corpo do fomulário
+        //console.log(req.body)
 
+         //inserir dados
+        const query = `INSERT INTO places (name,image,address,address2,state,city,items) VALUES (?,?,?,?,?,?,?);`
         const values = [
-            req.body.image,
             req.body.name,
+            req.body.image,
             req.body.address,
             req.body.address2,
             req.body.state,
             req.body.city,
-            req.body.items
+            req.body.items,
         ]
-
-        function afterInsertData(err) {
-            if(err) {
+        function affterInsertData(err) {
+            if (err){
                 console.log(err)
-                return res.send("Erro no cadastro!")
+                return res.send("Erro de Cadastro")
             }
-            console.log("Cadastrado com sucesso")
+            console.log ("Cadastrado com Sucesso")
             console.log(this)
+            return res.render("create-point.html", {saved: true})
+        }
+        db.run(query, values, affterInsertData)
+    })
+
+    server.get("/search", (req, res) => {
+
+        const search = req.query.search
+        if (search=="") {
+            return res.render("search-results.html", {total:0})            
         }
 
-        db.run(query, values, afterInsertData)
+        //pegar os dados no banco de dados para exibir no html
+        db.all(`SELECT * FROM places`, function(err,rows){
 
-
-        db.all(`SELECT * FROM places`,function (err, rows) {
-            if (err) {
+            if (err){
                 return console.log(err)
             }
-
-            console.log("Aqui estão seus registros: ")
+            const total = rows.length
+            //console.log("Registros encontrados")
             console.log(rows)
-
-            return  res.render("create-point.html", { saved: true })
+            return res.render("search-results.html", {places: rows, total})    
         })
 
-})
-
-server.get("/search", (req, res) => {
-    const search = req.query.search
-    if (search == "" ){
-        return res.render("search-results.html", { total: 0 })
-    }
-
-    db.all(`SELECT * FROM places WHERE LIKE '%${search}%'`, function (err, rows) {
-        if (err) {
-            return console.log(err)
-        }
-
-        const total = rows.length
-
-        return res.render("search-results.html", { places: rows, total: total})
+        
     })
-})
-
+// ligar o servidor
 server.listen(3000)
